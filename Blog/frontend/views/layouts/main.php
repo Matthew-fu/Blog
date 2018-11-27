b890f8242b16bac85d2f832a7fa539e86e88c11d
Matthew-vu <matthewfu1993@gmail.com>
2018-11-27 22:00:46 +0800

    test

--- Blog/frontend/views/layouts/main.php
+++ Blog/frontend/views/layouts/main.php
@@ -15,16 +15,9 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
+<?php  define('LAYOUTS', 'main'); include 'header.php'; ?>
 <body>
-<?php $this->beginBody() ?>
+
 
 <div class="wrap">
     <?php
@@ -68,16 +61,6 @@ AppAsset::register($this);
         <?= $content ?>
     </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
-
-<?php $this->endBody() ?>
+<?php include 'footer.php'; ?>
 </body>
 </html>
-<?php $this->endPage() ?>
